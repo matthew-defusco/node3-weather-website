1726dca79e4fa90a468fbad0cf1269141c04781f
Matthew DeFusco <matthew.defusco@gmail.com>
2021-02-13 18:02:53 -0500

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -49,14 +50,22 @@ app.get('/weather', (req, res) => {
   }
 
   const address = req.query.address
-  geocode(address, (error, { latitude, longitude, location } = {}) => {
+  geocode(address, (error, {
+    latitude,
+    longitude,
+    location
+  } = {}) => {
     if (error) {
-      return res.send({ error })
+      return res.send({
+        error
+      })
     }
 
     forecast(latitude, longitude, (error, forecastData) => {
       if (error) {
-        return res.send({ error })
+        return res.send({
+          error
+        })
       }
       res.send({
         location,
@@ -95,6 +104,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+app.listen(port, () => {
+  console.log(`Server is up on port${port}`)
 })
